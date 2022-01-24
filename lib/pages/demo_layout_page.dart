af78fc1f4ee209f6ea767c99bc132d08a0c3d7ab
LittleFlyers <2538285284@qq.com>
2022-01-25 00:02:26 +0800

    Flow布局

--- lib/pages/demo_layout_page.dart
+++ lib/pages/demo_layout_page.dart
@@ -20,6 +20,11 @@ class DemoLayoutPage extends StatelessWidget {
               return _FlexibleLayout();
             }));
           }),
+          BaseButton("流式布局", () {
+            Navigator.push(context, new MaterialPageRoute(builder: (context) {
+              return _FlowLayoutPage();
+            }));
+          }),
         ]),
       ),
     );
@@ -151,11 +156,12 @@ class _FlexibleLayout extends StatelessWidget {
                   ],
                 )),
             Container(
-                margin: EdgeInsets.all(6.0), child: TitleText("Flex平均分配高度")),
+                margin: EdgeInsets.only(top: 6.0),
+                child: TitleText("Flex平均分配高度")),
             Container(
-              margin: EdgeInsets.all(6.0),
+              margin: EdgeInsets.only(left: 6.0, right: 6.0),
               child: Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
+                  padding: const EdgeInsets.only(top: 6.0),
                   child: SizedBox(
                       height: 90.0,
                       child: Flex(
@@ -186,12 +192,12 @@ class _FlexibleLayout extends StatelessWidget {
                       ))),
             ),
             Container(
-                margin: EdgeInsets.all(6.0),
+                margin: EdgeInsets.only(top: 6.0),
                 child: TitleText("Flex按照3:2:1高度分配")),
             Container(
-              margin: EdgeInsets.all(6.0),
+              margin: EdgeInsets.only(left: 6.0, right: 6.0),
               child: Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
+                  padding: const EdgeInsets.only(top: 6.0),
                   child: SizedBox(
                       height: 90.0,
                       child: Flex(
@@ -225,5 +231,100 @@ class _FlexibleLayout extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _FlowLayoutPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FlowLayoutState();
+  }
+}
+
+class _FlowLayoutState extends State<_FlowLayoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    List<Color> colors = const <Color>[
+      Colors.amberAccent,
+      Colors.tealAccent,
+      Colors.black38,
+      Colors.red,
+      Colors.indigo,
+      Colors.purple,
+      Colors.orange,
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("流式布局"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(margin: EdgeInsets.all(6.0), child: TitleText("Wrap布局")),
+            Wrap(
+              spacing: 6.0,
+              runSpacing: 4.0,
+              alignment: WrapAlignment.center,
+              children: [
+                for (int i = 0; i < 7; i++)
+                  Container(
+                    width: 60.0,
+                    height: 40.0,
+                    color: Colors.amberAccent,
+                  )
+              ],
+            ),
+            Container(margin: EdgeInsets.all(6.0), child: TitleText("Flow布局")),
+            Flow(
+              delegate: _TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+              children: [
+                for (int i = 0; i < 7; i++)
+                  Container(
+                    width: 90.0,
+                    height: 90.0,
+                    color: colors[i],
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin = EdgeInsets.zero;
+
+  _TestFlowDelegate({this.margin});
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var left = margin.left;
+    var top = margin.top;
+    for (int i = 0; i < context.childCount; i++) {
+      var childPosition = context.getChildSize(i).width + left + margin.right;
+      if (childPosition < context.size.width) {
+        context.paintChild(i,
+            transform: Matrix4.translationValues(left, top, 0.0));
+        left = childPosition + margin.left;
+      } else {
+        left = margin.left;
+        top += context.getChildSize(i).height + margin.top + margin.bottom;
+        context.paintChild(i,
+            transform: Matrix4.translationValues(left, top, 0.0));
+        left += context.getChildSize(i).width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant FlowDelegate oldDelegate) {
+    return oldDelegate != this;
+  }
+
+  @override
+  getSize(BoxConstraints constraints) {
+    return Size(double.infinity, 300.0);
   }
 }
