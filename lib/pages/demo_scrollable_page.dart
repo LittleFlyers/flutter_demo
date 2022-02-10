c782306621adeef3636832acff928daafe9e7580
LittleFlyers <2538285284@qq.com>
2022-02-10 15:15:50 +0800

    去除卡片使用demo

--- lib/pages/demo_scrollable_page.dart
+++ lib/pages/demo_scrollable_page.dart
@@ -11,12 +11,6 @@ class DemoScrollablePage extends StatelessWidget {
       body: Center(
         child: Column(children: <Widget>[
           BaseButton("线性布局", () {}),
-          CardWidget(
-            "https://pics6.baidu.com/feed/8435e5dde71190efbf0861357e15d610fcfa607a.jpeg?token=c8fa6ccdc549e2c0d07981ef60476599&s=AA093A6E4D1044558827EA6A0300607B",
-            contentText: "这是一只小猫",
-            width: 200,
-            userName: "月影风情",
-          ),
         ]),
       ),
     );
