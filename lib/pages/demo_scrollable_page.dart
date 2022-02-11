f865e7aa447d3d3ad20a18f309f6ff9a596b2e83
LittleFlyers <2538285284@qq.com>
2022-02-11 16:31:32 +0800

    完成GridView开发

--- lib/pages/demo_scrollable_page.dart
+++ lib/pages/demo_scrollable_page.dart
@@ -21,6 +21,11 @@ class DemoScrollablePage extends StatelessWidget {
               return _ListViewPage();
             }));
           }),
+          BaseButton("GridView", () {
+            Navigator.push(context, new MaterialPageRoute(builder: (context) {
+              return _GridViewPage();
+            }));
+          }),
         ]),
       ),
     );
@@ -101,6 +106,120 @@ class _ListViewState extends State<_ListViewPage> {
   void _refreshData() {
     Future.delayed(Duration(seconds: 1)).then((e) {
       _cards.insertAll(_cards.length - 1, _cardFactory.createCards(200.0, 8));
+      setState(() {});
+    });
+  }
+}
+
+class _GridViewPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _GridViewState();
+  }
+}
+
+class _GridViewState extends State<_GridViewPage> {
+  List<Widget> _cards = <Widget>[
+    Container(
+        padding: const EdgeInsets.all(16.0),
+        alignment: Alignment.center,
+        child: SizedBox(
+            width: 24.0,
+            height: 24.0,
+            child: CircularProgressIndicator(strokeWidth: 2.0)))
+  ];
+  CardFactory _cardFactory = CardFactory();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("GridView"),
+        ),
+        body: Container(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 1.0),
+            itemCount: _cards.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (_cards[index] is Container) {
+                _refreshData();
+              }
+              return _cards[index];
+            },
+          ),
+        ));
+  }
+
+  //模拟加载，每次加载8张卡片
+  void _refreshData() {
+    Future.delayed(Duration(seconds: 1)).then((e) {
+      _cards.insertAll(_cards.length - 1, <Widget>[
+        Container(
+          child: Text("1"),
+          color: Colors.black26,
+        ),
+        Container(
+          child: Text("2"),
+          color: Colors.amber,
+        ),
+        Container(
+          child: Text("3"),
+          color: Colors.blue,
+        ),
+        Container(
+          child: Text("4"),
+          color: Colors.brown,
+        ),
+        Container(
+          child: Text("5"),
+          color: Colors.deepOrangeAccent,
+        ),
+        Container(
+          child: Text("6"),
+          color: Colors.blueGrey,
+        ),
+        Container(
+          child: Text("7"),
+          color: Colors.deepPurple,
+        ),
+        Container(
+          child: Text("8"),
+          color: Colors.pinkAccent,
+        ),
+        Container(
+          child: Text("9"),
+          color: Colors.white54,
+        ),
+        Container(
+          child: Text("10"),
+          color: Colors.greenAccent,
+        ),
+        Container(
+          child: Text("11"),
+          color: Colors.yellowAccent,
+        ),
+        Container(
+          child: Text("12"),
+          color: Colors.lime,
+        ),
+        Container(
+          child: Text("13"),
+          color: Colors.purpleAccent,
+        ),
+        Container(
+          child: Text("14"),
+          color: Colors.deepPurpleAccent,
+        ),
+        Container(
+          child: Text("15"),
+          color: Colors.indigo,
+        ),
+        Container(
+          child: Text("16"),
+          color: Colors.white,
+        ),
+      ]);
       setState(() {});
     });
   }
